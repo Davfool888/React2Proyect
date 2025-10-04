4f3e0959f81770695207a4719f6e8cce7e15b2bd
Davfool888 <davfool888@gmail.com>
2025-10-04 18:04:33 -0500

    UseEffect, y llamar datos

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,55 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
-import {Button} from './components/index'
 
 
-const style = {
-    button: {
-      color: "blue",
-    backgroundColor: "grey"
-    }
-    
-  }
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState("Santiago")
+  const [data, setData] = useState([])
+  const [loading, setLoanding] = useState(false)
+  const [error, setError] = useState("")
 
-const countMore = () =>{
-  setCount((count) => count +1)
-  setCount((count) => count +1)
-  setCount((count) => count +1)
 
-}
+  const consoleLoader = useCallback((loadingValue: boolean) => {
+    setLoanding(loadingValue)
+    console.info(loading)
+  },[loading] )
 
-const changeName= () =>{
-  setName("David")
-}
+  const fetchData = useCallback(
+    async () => {
+    consoleLoader(true)
+    try {
+      const response = await fetch("https://api.example,com/data")
+      
+      if (!response.ok) {
+        throw new Error("Error al optener datos")
+      }
+
+      const jsonData = await response.json()
+      setData(jsonData)
+    } catch (err) {
+      setError(err as string)
+    } finally {
+      setLoanding(false)
+    }
+  }, 
+  [consoleLoader],
+  ) 
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  if (loading) {
+    return <div>Cargando-----</div>
+  }
+
+  if (error) {
+    return <div>Ups! Hay un error: {error}</div>
+  }
 
   return (
-    <>
-      <div>
-        <Button label={`count is ${count}`} parentMethod={ countMore} />
-
-        </div>
-    </>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 
